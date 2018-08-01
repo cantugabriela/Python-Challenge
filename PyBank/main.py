d38a95b2da7933b53234404750983aa746597c2b
gcantu <39506132+cantugabriela@users.noreply.github.com>
2018-08-01 16:43:20 -0500

    Added the average change in profit value

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,21 +3,28 @@ import os, csv
 from pathlib import Path 
 
 # filepath 
-csv_file_path = Path('python-challenge\\Resources\\budget_data.csv')
+csv_file_path = Path('python-challenge\\PyBank\\budget_data.csv')
 
 #variables
-total_months = 0 
-total_profit = 0
-previous_profit = 0
-profit_change = 0
-profits = []
+total_months = []
+total_profit = []
+change_profit = []
+
     
 with open(csv_file_path,newline="", encoding='utf-8') as budget:
-    csvreader = csv.reader(budget,delimiter=",")   
+    csvreader = csv.reader(budget,delimiter=",") 
+    header = next(csvreader)  
 
     for row in csvreader: 
-        total_months = total_months + 1
-        total_profit = total_profit 
-    print(total_months)
-    print(total_profit)
-    print(total_profit)
+        total_months.append(row[0])
+        total_profit.append(int(row[1]))
+    
+    for i in range(len(total_profit)-1):
+        change_profit.append(total_profit[i+1]-total_profit[i])
+    
+
+print(len(total_months))
+print(sum(total_profit))
+print(sum(change_profit)/len(change_profit))
+
+
