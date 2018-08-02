2d3cdd47980e731e09f66a332dcae9bbbf5254a7
gcantu <39506132+cantugabriela@users.noreply.github.com>
2018-08-01 22:50:53 -0500

    Raw correct values

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,30 +1,64 @@
-# import libraries 
+# Import Libraries 
 import os, csv
 from pathlib import Path 
 
-# filepath 
+# Declare file location through pathlib
 csv_file_path = Path('python-challenge\\PyBank\\budget_data.csv')
 
-#variables
+# Create empty lists to iterate through specific rows for the following variables
 total_months = []
 total_profit = []
-change_profit = []
+monthly_profit_change = []
 
+# Create empty dictionaries to look up greatest increase/decrease corresponding months
+profit_and_months = {}
+profit_and_change = {}
     
+# Open the file in the default read mode 
 with open(csv_file_path,newline="", encoding='utf-8') as budget:
+
+     # Store the contents of budget_data.csv in the variable csvreader
     csvreader = csv.reader(budget,delimiter=",") 
+
+    # Skip the header labels to iterate with the values
     header = next(csvreader)  
 
+    # Iterate through the rows in the stored file contents
     for row in csvreader: 
+
+        # Create an appended the total months and total profit to their corresponding lists
         total_months.append(row[0])
         total_profit.append(int(row[1]))
-    
-    for i in range(len(total_profit)-1):
-        change_profit.append(total_profit[i+1]-total_profit[i])
-    
 
+        # Store the dictionary with the profit as key and month as value
+        profit_and_months[row[1]] = row[0]
+
+    # Iterate through the profits in order to get the monthly change in profits
+    for i in range(len(total_profit)-1):
+        
+        # Take the difference between two months and append to monthly profit change
+        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
+        
+        # Store the values to the profit and change dictionary 
+        # Profit and change is the key and Profit is the value
+        profit_and_change[total_profit[i+1]-total_profit[i]]=total_profit[i+1]
+
+# Obtain the max and min of the the montly profit change list
+max_increase = max(monthly_profit_change)  
+max_decrease = min(monthly_profit_change) 
+
+
+greatest_increase = profit_and_change[max_increase]
+greatest_decrease = profit_and_change[max_decrease]
+
+max_inc = profit_and_months[str(greatest_increase)]
+max_dec = profit_and_months[str(greatest_decrease)]
+
+#Print Statements
+print("Financial Analysis")
+print("----------------------------")
 print(len(total_months))
 print(sum(total_profit))
-print(sum(change_profit)/len(change_profit))
-
-
+print(sum(monthly_profit_change)/len(monthly_profit_change))
+print("Greatest Increase in Profits: " + max_inc + " ($" + str(max_increase)+")")
+print("Greatest Decrease in Profits: " + max_dec + " ($" + str(max_decrease)+")")
