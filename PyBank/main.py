115e69a992be3c15df006be88425f69e542c7925
gcantu <39506132+cantugabriela@users.noreply.github.com>
2018-08-03 19:46:52 -0500

    Optimized algorithm with lists

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,21 +1,16 @@
-# Dependencies
+# Import Dependencies
 import os, csv
 from pathlib import Path 
 
 # Declare file location through pathlib
-input_file = Path("python-challenge\\PyBank\\budget_data.csv")
-output_file = Path("python-challenge\\PyBank\\Financial_Analysis_Summary.txt")
+input_file = Path("python-challenge", "PyBank", "budget_data.csv")
 
 # Create empty lists to iterate through specific rows for the following variables
 total_months = []
 total_profit = []
 monthly_profit_change = []
-
-# Create empty dictionaries to look up greatest increase/decrease corresponding months
-profit_and_months = {}
-profit_and_change = {}
-    
-# Open the file in the default read mode 
+ 
+# Open csv in default read mode with context manager
 with open(input_file,newline="", encoding="utf-8") as budget:
 
      # Store the contents of budget_data.csv in the variable csvreader
@@ -27,12 +22,9 @@ with open(input_file,newline="", encoding="utf-8") as budget:
     # Iterate through the rows in the stored file contents
     for row in csvreader: 
 
-        # Create an appended the total months and total profit to their corresponding lists
+        # Append the total months and total profit to their corresponding lists
         total_months.append(row[0])
         total_profit.append(int(row[1]))
-
-        # Store the dictionary with the profit as key and month as value
-        profit_and_months[row[1]] = row[0]
 
     # Iterate through the profits in order to get the monthly change in profits
     for i in range(len(total_profit)-1):
@@ -40,35 +32,27 @@ with open(input_file,newline="", encoding="utf-8") as budget:
         # Take the difference between two months and append to monthly profit change
         monthly_profit_change.append(total_profit[i+1]-total_profit[i])
         
-        # Store the values to the profit and change dictionary 
-        # Profit and change is the key and Profit is the value
-        profit_and_change[total_profit[i+1]-total_profit[i]]=total_profit[i+1]
-
 # Obtain the max and min of the the montly profit change list
-max_increase = max(monthly_profit_change)  
-max_decrease = min(monthly_profit_change) 
+max_increase_value = max(monthly_profit_change)
+max_decrease_value = min(monthly_profit_change)
 
-
-greatest_increase = profit_and_change[max_increase]
-greatest_decrease = profit_and_change[max_decrease]
-
-max_inc = profit_and_months[str(greatest_increase)]
-max_dec = profit_and_months[str(greatest_decrease)]
+# Correlate max and min to the proper month using month list and index from max and min
+#We use the plus 1 at the end since month associated with change is the + 1 month or next month
+max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
+max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
 
 #Print Statements
-
 
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {len(total_months)}")
 print(f"Total: ${sum(total_profit)}")
 print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
-print(f"Greatest Increase in Profits: {max_inc} (${(str(max_increase))})")
-print(f"Greatest Decrease in Profits: {max_dec} (${(str(max_decrease))})")
-
-
+print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
 
 #Output files
+output_file = Path("python-challenge", "PyBank", "Financial_Analysis_Summary.txt")
 
 with open(output_file,"w") as file:
 #Write methods to print to Financial_Analysis_Summary 
@@ -82,9 +66,8 @@ with open(output_file,"w") as file:
     file.write("\n")
     file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
     file.write("\n")
-    file.write(f"Greatest Increase in Profits: {max_inc} (${(str(max_increase))})")
+    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
     file.write("\n")
-    file.write(f"Greatest Decrease in Profits: {max_dec} (${(str(max_decrease))})")
-
+    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
 
 
