8558b6a78a9f5c09e1a1129dc7d944268629dc7f
gcantu <39506132+cantugabriela@users.noreply.github.com>
2018-08-03 19:50:51 -0500

    Formatting improved

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,17 +1,17 @@
-# Import libraries 
+# Import dependencies
 import os, csv
 from pathlib import Path 
 
-#Variables 
+# Assign file location with the pathlib library
+csv_file_path = Path("python-challenge", "PyPoll", "election_data.csv")
+
+
+#Declare Variables 
 total_votes = 0 
 khan_votes = 0
 correy_votes = 0
 li_votes = 0
 otooley_votes = 0
-# Assign file location with the pathlib library
-csv_file_path = Path("python-challenge\\PyPoll\\election_data.csv")
-output_file = Path("python-challenge\\PyPoll\\Election_Results_Summary.txt")
-
 
 # Open csv in default read mode with context manager
 with open(csv_file_path,newline="", encoding='utf-8') as elections:
@@ -21,24 +21,25 @@ with open(csv_file_path,newline="", encoding='utf-8') as elections:
 
     #Skip the header so we iterate through the actual values
     header = next(csvreader)     
+
     # Iterate through each row in the csv
     for row in csvreader: 
 
         # Count the unique Voter ID's and store in variable  called total_votes
-        total_votes = total_votes + 1
+        total_votes +=1
 
         # We have four candidates if the name is found, count the times it appears and store in a list
         # We can use this values in our percent vote calculation in the print statements
         if row[2] == "Khan": 
-            khan_votes = khan_votes + 1
+            khan_votes +=1
         elif row[2] == "Correy":
-            correy_votes = correy_votes + 1
+            correy_votes +=1
         elif row[2] == "Li": 
-            li_votes = li_votes + 1
+            li_votes +=1
         elif row[2] == "O'Tooley":
-            otooley_votes = otooley_votes + 1
+            otooley_votes +=1
 
-    # To find the winner we want to make a dictionary out of the two lists we previously created 
+ # To find the winner we want to make a dictionary out of the two lists we previously created 
 candidates = ["Khan", "Correy", "Li","O'Tooley"]
 votes = [khan_votes, correy_votes,li_votes,otooley_votes]
 
@@ -51,9 +52,9 @@ key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
 # Print a the summary of the analysis
 
 khan_percent = (khan_votes/total_votes) *100
-correy_percent = round(((correy_votes/total_votes) * 100), 3)
-li_percent = round(((li_votes/total_votes)* 100), 3)
-otooley_percent = round(((otooley_votes/total_votes) * 100),3)
+correy_percent = (correy_votes/total_votes) * 100
+li_percent = (li_votes/total_votes)* 100
+otooley_percent = (otooley_votes/total_votes) * 100
 
 # Print the summary table
 print(f"Election Results")
@@ -68,11 +69,14 @@ print(f"----------------------------")
 print(f"Winner: {key}")
 print(f"----------------------------")
 
-
 #Output files
 
+# Assign output file location and with the pathlib library
+output_file = Path("python-challenge", "PyPoll", "Election_Results_Summary.txt")
+
 with open(output_file,"w") as file:
-##Write methods to print to Elections_Results_Summary 
+
+#Write methods to print to Elections_Results_Summary 
     file.write(f"Election Results")
     file.write("\n")
     file.write(f"----------------------------")
