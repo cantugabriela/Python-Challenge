2165c8b1b399a467c14caecd1131caf1605a3871
gcantu <39506132+cantugabriela@users.noreply.github.com>
2018-08-09 18:01:46 -0500

    Added pyboss paragraph analyzer

---
+++ PyParagraph(Bonus)/main.py
@@ -0,0 +1,55 @@
+# Import Dependencies 
+import os
+from pathlib import Path 
+
+# Read file, change between "paragraph_1" or "paragraph_2 below"
+txt_file_path =Path("python-challenge", "PyParagraph(Bonus)", "paragraph_1.txt")
+
+# Open file in read mode 
+with open(txt_file_path) as text:
+    paragraph = text.read()
+
+# Use a list comprehension to split the the text file into words using space as the delimeter and return the appended array "words"
+# Obtain length of array 
+words = len([w for w in paragraph.split(sep=" ")])
+
+# Use a list comprehension to split the text file into sentences by using period as the delimeter and return the appended array "sentences"
+# Obtain length of array 
+sentences = len([w for w in paragraph if w is "."]) + len([w for w in paragraph if w is "?"]) +  len([w for w in paragraph if w is "!"])
+
+# Calculate the average letters in words
+letter_avg_per_word = len([c for c in paragraph if c.isalpha()])/words
+
+# Calculate the average words in sentences 
+avg_words_per_sentence = words/sentences
+
+# Print a statement 
+print(f"Paragraph Analysis")
+print(f"-----------------")
+print(f"Approximate Word Count: {words}")
+print(f"Approximate Sentence Count: {sentences}")
+print(f"Average Letter Count: {letter_avg_per_word}")
+print(f"Average Sentence Length: {avg_words_per_sentence}")
+
+
+# Output files, change output name if needed 
+output_file = Path("python-challenge", "PyParagraph(Bonus)", "paragraph_1_analysis.txt")
+
+with open(output_file,"w") as file:
+    
+# Write methods to export paragraph analysis
+    file.write("\n")
+    file.write(f"Paragraph Analysis")
+    file.write("\n")
+    file.write(f"-----------------")
+    file.write("\n")
+    file.write(f"Approximate Word Count: {words}")
+    file.write("\n")
+    file.write(f"Approximate Sentence Count: {sentences}")
+    file.write("\n")
+    file.write(f"Average Letter Count: {letter_avg_per_word}")
+    file.write("\n")
+    file.write(f"Average Sentence Length: {avg_words_per_sentence}")
+
+
+
